902e78118baaad23710ab35391cc1ce8e5b1be26
Shashank <varshneys495@gmail.com>
2025-07-21 19:48:25 +0530

    Integrated-itemsTotalCounter

--- src/Pages/Cart.js
+++ src/Pages/Cart.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
+import { remove } from '../Store/cartSlice';
+import { useDispatch } from 'react-redux';
 
+// Here we bring the products from the cartSlice (Redux)
 const Cart = () => {
+  const products = useSelector((state) => state.cart);
+  const dispatch = useDispatch ();
+  const HandleRemove = (product) => {
+    dispatch(remove(product.id));
+    alert(`${product.title} removed from cart`);
+  }
   return (
-    <div>Cart</div>
+    products.length === 0 ? (
+      <img src='/empty-cart.png' alt='Empty cart' className='emptyCartImage'/>
+    ) : (
+      <div className='cartWrapper'>
+        {products.map(product => (
+          <div className='cartCard' key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <h4>{product.title}</h4>
+            <h5>Price: ${product.price}</h5>
+            <button onClick={() => {HandleRemove(product)}} className='btn'>Remove from Cart</button>
+          </div>
+        ))}
+      </div>
+    )
   )
 }
 
